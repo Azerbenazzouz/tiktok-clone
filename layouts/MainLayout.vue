7faf14a7e2fe15abb9a5c24873f1dfa5cf94352c
Azer Ben Azzouz <benazzouzazer@gmail.com>
2025-01-18 10:00:51 +0100

    Ajout de la configuration de Tailwind CSS, création de la structure de mise en page avec MainLayout et TopNav, et mise à jour de la configuration Nuxt pour intégrer les nouveaux composants.

---
+++ layouts/MainLayout.vue
@@ -0,0 +1,11 @@
+<template>
+    <TopNav/>
+    <div>
+    </div>
+</template>
+
+<script setup>
+import TopNav from '~/Components/TopNav.vue';
+
+
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {}
+    }
+  },
+  css: ['~/assets/css/main.css'],
+  modules: [
+    '@nuxt/icon',
+    '@pinia/nuxt',
+    'pinia-plugin-persistedstate/nuxt',
+  ],
+  pages: true,
 })
