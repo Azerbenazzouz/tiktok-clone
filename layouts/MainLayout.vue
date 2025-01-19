970abb10aefb71ae7affc4ae74e3a7d71d4de884
Azer Ben Azzouz <benazzouzazer@gmail.com>
2025-01-19 13:07:52 +0100

    Ajout de la structure de mise en page avec SideNavMain, PostMain et TopNav, ainsi que des composants MenuItem et MenuItemFollow pour améliorer la navigation et l'affichage des publications.

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -1,11 +1,19 @@
 <template>
     <TopNav/>
-    <div>
+    <div
+        :class="route.fullPath === '/' ? 'max-w-[1140px]' : ''"
+        class="flex justify-between mx-auto w-full lg:px-2.5 px-0"
+    >
+        <div>
+            <SideNavMain/>
+        </div>
+            
+        <slot/>
     </div>
 </template>
 
 <script setup>
-import TopNav from '~/Components/TopNav.vue';
+const route = useRoute();
 
 
 </script>
